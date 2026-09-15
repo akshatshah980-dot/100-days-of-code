7a5b7906961572aa41830e5079ea8e1c3a1f48f7
akshatshah980-dot <akshatshah980@gmail.com>
2026-09-15 17:43:00 +0530

    Day 27 program

---
+++ classprog53.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+    int i,j;
+    for( i=1;i<=5;++i) //upper half of the diamond
+    {
+        for(j=1;j<=5-i;++j)
+        {
+            printf(" ");
+        }
+        for(j=1;j<=2*i-1;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(i=5-1;i>=1;--i) //LOWER HALF OF THE DIAMOND
+    {
+        for(j=1;j<=5-i;j++)
+        {
+            printf(" ");
+        }
+        for(j=1;j<=2*i-1;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+
+}
